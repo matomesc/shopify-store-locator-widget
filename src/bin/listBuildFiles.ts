19b4e7868538d5ced4920838a7e8a5163ba244dc
Mihai Tomescu <matomesc@gmail.com>
2024-06-29 20:11:49 +0000

    feat: list build files after building

---
+++ src/bin/listBuildFiles.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+
+let files: string[] = [];
+const buildDirectory = path.resolve(__dirname, '../../build');
+
+const jsDirectory = path.resolve(buildDirectory, 'static/js');
+const cssDirectory = path.resolve(buildDirectory, 'static/css');
+
+if (fs.existsSync(jsDirectory)) {
+  const jsFiles = fs
+    .readdirSync(jsDirectory)
+    .filter((file) => file.endsWith('.js'))
+    .map((file) => `js:${file}`);
+  files = [...files, ...jsFiles];
+}
+
+if (fs.existsSync(cssDirectory)) {
+  const cssFiles = fs
+    .readdirSync(cssDirectory)
+    .filter((file) => file.endsWith('.css'))
+    .map((file) => `css:${file}`);
+  files = [...files, ...cssFiles];
+}
+
+files.forEach((file) => {
+  console.log(file);
+});
