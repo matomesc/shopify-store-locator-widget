70ed52afbc3208d7f8e1db06c7d7c72f04a9ae62
Mihai Tomescu <matomesc@gmail.com>
2024-07-17 20:31:02 +0000

    feat: add keys to address component divs

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -9,43 +9,43 @@ export const Address: React.FC<AddressProps> = ({ location }) => {
   const pieces: React.ReactNode[] = [];
 
   if (location.address1) {
-    pieces.push(<div>{location.address1}</div>);
+    pieces.push(<div key="address1">{location.address1}</div>);
   }
 
   if (location.address2) {
-    pieces.push(<div>{location.address2}</div>);
+    pieces.push(<div key="address2">{location.address2}</div>);
   }
 
   if (location.city && location.state && location.zip) {
     pieces.push(
-      <div>
+      <div key="cityStateZip">
         {location.city}, {location.state} {location.zip}
       </div>,
     );
   } else if (location.city && location.state) {
     pieces.push(
-      <div>
+      <div key="cityState">
         {location.city}, {location.state}
       </div>,
     );
   } else if (location.city && location.zip) {
     pieces.push(
-      <div>
+      <div key="cityZip">
         {location.city}, {location.zip}
       </div>,
     );
   } else if (location.state && location.zip) {
     pieces.push(
-      <div>
+      <div key="stateZip">
         {location.state} {location.zip}
       </div>,
     );
   } else if (location.city) {
-    pieces.push(<div>{location.city}</div>);
+    pieces.push(<div key="city">{location.city}</div>);
   } else if (location.state) {
-    pieces.push(<div>{location.state}</div>);
+    pieces.push(<div key="state">{location.state}</div>);
   } else if (location.zip) {
-    pieces.push(<div>{location.zip}</div>);
+    pieces.push(<div key="zip">{location.zip}</div>);
   }
 
   return <div className="neutek-locator-address">{pieces}</div>;
