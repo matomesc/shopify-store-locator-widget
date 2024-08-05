0a2995a1d5d876a6b6d484c92c9723677004d936
Mihai Tomescu <matomesc@gmail.com>
2024-08-05 22:19:27 +0000

    feat: refactor locator dto

--- src/dto/api.ts
+++ src/dto/api.ts
@@ -1,81 +1,65 @@
-/* eslint-disable @typescript-eslint/no-redeclare */
-import { z } from 'zod';
-
-export const GetLocatorOutput = z.object({
-  ok: z.boolean(),
-  settings: z.object({
-    googleMapsApiKey: z.string(),
-  }),
-  searchFilters: z.array(
-    z.object({
-      id: z.string(),
-      name: z.string(),
-      position: z.number(),
-      enabled: z.boolean(),
-      showInList: z.boolean(),
-      showInMap: z.boolean(),
-    }),
-  ),
-  customFields: z.array(
-    z.object({
-      id: z.string(),
-      name: z.string(),
-      position: z.number(),
-      enabled: z.boolean(),
-      hideLabel: z.boolean(),
-      labelPosition: z.enum(['inline', 'top']),
-      showInList: z.boolean(),
-      showInMap: z.boolean(),
-      defaultValue: z.string(),
-    }),
-  ),
-  customActions: z.array(
-    z.object({
-      id: z.string(),
-      name: z.string(),
-      type: z.enum(['link', 'js']),
-      position: z.number(),
-      enabled: z.boolean(),
-      showInList: z.boolean(),
-      showInMap: z.boolean(),
-      defaultValue: z.string(),
-      openInNewTab: z.boolean(),
-    }),
-  ),
-  locations: z.array(
-    z.object({
-      id: z.string(),
-      name: z.string(),
-      active: z.boolean(),
-      phone: z.string(),
-      email: z.string(),
-      website: z.string(),
-      address1: z.string(),
-      address2: z.string(),
-      city: z.string(),
-      state: z.string(),
-      zip: z.string(),
-      country: z.string(),
-      lat: z.number(),
-      lng: z.number(),
-      searchFilters: z.array(z.object({ id: z.string() })),
-      customFieldValues: z.array(
-        z.object({
-          id: z.string(),
-          value: z.string(),
-          locationId: z.string(),
-          customFieldId: z.string(),
-        }),
-      ),
-      customActionValues: z.array(
-        z.object({
-          id: z.string(),
-          value: z.string(),
-          locationId: z.string(),
-          customActionId: z.string(),
-        }),
-      ),
-    }),
-  ),
-});
-export type GetLocatorOutput = z.infer<typeof GetLocatorOutput>;
+export type GetLocatorOutput = {
+  ok: boolean;
+  settings: {
+    googleMapsApiKey: string;
+  };
+  searchFilters: Array<{
+    id: string;
+    name: string;
+    position: number;
+    enabled: boolean;
+    showInList: boolean;
+    showInMap: boolean;
+  }>;
+  customFields: Array<{
+    id: string;
+    name: string;
+    position: number;
+    enabled: boolean;
+    hideLabel: boolean;
+    labelPosition: 'inline' | 'top';
+    showInList: boolean;
+    showInMap: boolean;
+    defaultValue: string;
+  }>;
+  customActions: Array<{
+    id: string;
+    name: string;
+    type: 'link' | 'js';
+    position: number;
+    enabled: boolean;
+    showInList: boolean;
+    showInMap: boolean;
+    defaultValue: string;
+    openInNewTab: boolean;
+  }>;
+  locations: Array<{
+    id: string;
+    name: string;
+    active: boolean;
+    phone: string;
+    email: string;
+    website: string;
+    address1: string;
+    address2: string;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+    lat: number;
+    lng: number;
+    searchFilters: Array<{ id: string }>;
+    customFieldValues: Array<{
+      id: string;
+      value: string;
+      locationId: string;
+      customFieldId: string;
+    }>;
+    customActionValues: Array<{
+      id: string;
+      value: string;
+      locationId: string;
+      customActionId: string;
+    }>;
+  }>;
+};
